55345fe7ba2f97cb4e5bf874e2595505e63d99fc
Mayank <mastermayank2000bgp@gmail.com>
2026-09-05 19:17:01 +0530

    Time: 0 ms (100%), Space: 44.6 MB (13.87%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for( String i : operations){
+            if(i.charAt(1)=='+'){
+                x++;
+            }
+            else x--;
+        }
+        return x;
+    }
+}
